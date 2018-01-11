9a531d296bf4032ef2bd089e9f2332162cc15df0
Varun Ramesh <varunramesh@live.com>
2018-01-10 18:41:45 -0800

    Bump up data expire time to 20 minutes.

--- server/common.js
+++ server/common.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const log = require('loglevel');
 
-// Chat data expires after five minutes.
-module.exports.DATA_EXPIRE = 5 * 60 * 1000;
+// Chat data expires after 20 minutes.
+module.exports.DATA_EXPIRE = 20 * 60 * 1000;
 
 // Recalculate the top charts every two seconds.
 module.exports.CALCULATE_TOP_CHATS_INTERVAL = 3 * 1000;
